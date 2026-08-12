1e15f4b7a05875650658019f763b8d8931b4521f
reham sherif <rehamsherrif@gmail.com>
2026-08-12 13:57:04 +0300

    added neon db link

--- Backend/model/db.js
+++ Backend/model/db.js
@@ -3,11 +3,10 @@ const { Pool } = require('pg');
 require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 5432),
-  database: process.env.DB_DATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
 const ensureUserAuthColumns = async () => {
