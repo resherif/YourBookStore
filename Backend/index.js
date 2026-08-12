b9555bf4298df10b087173a06c185c45b3d57934
reham sherif <rehamsherrif@gmail.com>
2026-08-12 13:51:10 +0300

    fix cors policy

--- Backend/index.js
+++ Backend/index.js
@@ -11,8 +11,8 @@ const { ensureUserAuthColumns } = require('./model/db');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173',
-    'http://127.0.0.1:5173',
+  origin: ['http://localhost:5173'||
+    'http://127.0.0.1:5173'||
     process.env.CLIENT_URL],
   credentials: true,
 }));
